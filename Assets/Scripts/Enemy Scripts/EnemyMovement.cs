5f566f71567b090edba6bad02b3071bd9bbeee72
Kieran <kieran.17@ichat.sp.edu.sg>
2023-02-18 19:29:17 +0800

    Added vertical enemies

--- Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -8,53 +8,9 @@ public class EnemyMovement : MonoBehaviour
     public float speed;
     public int patrolDestination;
 
-    public Transform playerTransform;
-/*    public bool isChasing;
-    public float chaseDistance;*/
-
     // Update is called once per frame
     void Update()
     {
-        /*        if (isChasing)
-                {
-                    if(transform.position.x > playerTransform.position.x)
-                    {
-                        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                        transform.position += Vector3.left * speed * Time.deltaTime;
-                    }
-                    if (transform.position.x < playerTransform.position.x)
-                    {
-                        transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
-                        transform.position += Vector3.right * speed * Time.deltaTime;
-                    }
-                }
-                else
-                {
-                    if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
-                    {
-                        isChasing = true;
-                    }
-
-                    if (patrolDestination == 0)
-                    {
-                        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
-                        if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
-                        {
-                            Flip();
-                            patrolDestination = 1;
-                        }
-                    }
-
-                    if (patrolDestination == 1)
-                    {
-                        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
-                        if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
-                        {
-                            Flip();
-                            patrolDestination = 0;
-                        }
-                    }
-                }*/
         if (patrolDestination == 0)
         {
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
