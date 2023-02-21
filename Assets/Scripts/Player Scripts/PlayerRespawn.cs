94f76ca800afa75188300376190015fa4c1a56ec
DarrenLYH <darren0lyh@gmail.com>
2023-02-21 21:59:40 +0800

    Alpha ver. 0.1 with level 1

--- Assets/Scripts/Player Scripts/PlayerRespawn.cs
+++ Assets/Scripts/Player Scripts/PlayerRespawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //temp
 
 public class PlayerRespawn : MonoBehaviour
 {
@@ -32,6 +33,12 @@ public class PlayerRespawn : MonoBehaviour
         if (collision.tag == "Checkpoint")
         {
             respawnPoint = collision.transform.position;
+        }
+
+        //temp
+        if (collision.tag == "Goal")
+        {
+            SceneManager.LoadScene("End Screen");
         }
     }
 }
