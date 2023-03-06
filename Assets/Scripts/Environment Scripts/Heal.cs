b3e42948cd906412ad07288b440076036c0d886a
Kieran <kieran.17@ichat.sp.edu.sg>
2023-03-06 19:52:19 +0800

    revamped lvl 1

--- Assets/Scripts/Environment Scripts/Heal.cs
+++ Assets/Scripts/Environment Scripts/Heal.cs
@@ -7,6 +7,19 @@ public class Heal : MonoBehaviour
 {
     public PlayerHealth playerHealth;
 
+    public float speed = 0.2f;
+    private float initialY;
+
+    private void Start()
+    {
+        initialY = gameObject.transform.position.y;
+    }
+    public void Update()
+    {
+        float y = Mathf.PingPong(Time.time * speed, 0.2f);
+        gameObject.transform.localPosition = new Vector3(gameObject.transform.position.x, initialY+y, gameObject.transform.position.z);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
